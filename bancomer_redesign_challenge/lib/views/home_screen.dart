b7b38500d5af7354a70cf8cea126ed34e758a1a4
Christopher Leal <cleal@dbmenos.com>
2021-01-15 12:53:41 -0600

    home page finished

--- bancomer_redesign_challenge/lib/views/home_screen.dart
+++ bancomer_redesign_challenge/lib/views/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bancomer_redesign_challenge/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -10,15 +12,250 @@ class HomeScreen extends StatelessWidget {
       fit: StackFit.expand,
       children: [
         _Background(),
-        Positioned(
-          left: 10.0,
-          right: 10.0,
-          top: size.height * 0.15,
-          height: size.height * 0.35,
-          child: _MyAccounts(),
-        ),
+        SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: SizedBox(
+              height: size.height * 1.35,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 5.0,
+                    right: 5.0,
+                    top: size.height * 0.15,
+                    height: size.height * 0.35,
+                    child: _MyAccounts(),
+                  ),
+                  Positioned(left: 0.0, right: 0.0, top: size.height * 0.52, height: size.height * 0.18, child: _ActionList()),
+                  Positioned(
+                    left: 0.0,
+                    right: 0.0,
+                    top: size.height * 0.72,
+                    height: size.height * 0.5,
+                    child: _MyCards(),
+                  ),
+                ],
+              ),
+            )),
+        Positioned(left: 0.0, right: 0.0, bottom: 0.0, height: kToolbarHeight, child: _BottomNavBar())
       ],
     ));
+  }
+}
+
+class _MyCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      color: BBVAColors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        child: Column(
+          children: [
+            Text(
+              'TUS TARJETAS',
+              textAlign: TextAlign.start,
+              style: TextStyle(color: BBVAColors.primary, fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            // const SizedBox(height: 30),
+            Expanded(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(index == 0 ? 15.0 : 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.rotate(
+                          angle: vector.radians(index == 0 ? 0 : 270),
+                          child: Container(
+                            height: size.height * 0.25,
+                            width: size.width * 0.65,
+                            decoration: BoxDecoration(color: BBVAColors.primary, borderRadius: BorderRadius.circular(15)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset(
+                                      'assets/bbva.png',
+                                      color: BBVAColors.white,
+                                      width: size.width * 0.2,
+                                    ),
+                                    Image.asset(
+                                      'assets/chip.png',
+                                      width: size.width * 0.1,
+                                    ),
+                                    Text(
+                                      '*62396',
+                                      style: TextStyle(color: BBVAColors.white, fontSize: 18),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                                  child: Image.asset(
+                                    'assets/visa.png',
+                                    color: BBVAColors.white,
+                                    width: size.width * 0.15,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        if (index == 0)
+                          Expanded(
+                            child: Container(
+                              width: size.width * 0.65,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 18.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.credit_card,
+                                          size: 20,
+                                          color: BBVAColors.primary,
+                                        ),
+                                        Text(
+                                          'Tarjeta digital',
+                                          style: TextStyle(fontWeight: FontWeight.w300, color: BBVAColors.primary, fontSize: 13),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Desactivar',
+                                          style: TextStyle(fontWeight: FontWeight.w300, color: BBVAColors.primary, fontSize: 13),
+                                        ),
+                                        Switch(
+                                          value: false,
+                                          onChanged: (value) {},
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BottomNavBar extends StatelessWidget {
+  const _BottomNavBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: BBVAColors.white, borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.home_filled,
+                color: BBVAColors.primary,
+              ),
+              Container(
+                height: 5.0,
+                width: 5.0,
+                decoration: BoxDecoration(color: BBVAColors.primary, shape: BoxShape.circle),
+              )
+            ],
+          )),
+          Expanded(
+              child: Icon(
+            Icons.favorite_border,
+            color: BBVAColors.primaryLight,
+          )),
+          Expanded(
+              child: Icon(
+            Icons.add_circle_outline,
+            color: BBVAColors.primaryLight,
+          )),
+          Expanded(
+              child: Icon(
+            Icons.mail_outline,
+            color: BBVAColors.primaryLight,
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class _ActionList extends StatelessWidget {
+  final List<_ActionListItem> actions = [
+    _ActionListItem(color: Color(0xFFF7893A), icon: Icons.add, text: 'Oportunidades'),
+    _ActionListItem(color: Color(0xFF4CABCE), icon: Icons.swap_horiz, text: 'Transferir'),
+    _ActionListItem(color: Color(0xFF49D17C), icon: Icons.attach_money, text: 'Retiro sin tarjeta'),
+    _ActionListItem(color: Color(0xFF735FDA), icon: Icons.build, text: 'Pagar servicio'),
+    _ActionListItem(color: Color(0xFFDA5F84), icon: Icons.call, text: 'Recargas'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: BBVAColors.white,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: actions.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final item = actions[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RawMaterialButton(
+                  padding: const EdgeInsets.all(10.0),
+                  fillColor: item.color,
+                  shape: CircleBorder(),
+                  child: Icon(
+                    item.icon,
+                    color: BBVAColors.white,
+                    size: 30.0,
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  item.text,
+                  style: TextStyle(color: BBVAColors.primaryLight, fontSize: 15),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
 
@@ -169,4 +406,11 @@ class _Background extends StatelessWidget {
       ],
     );
   }
+}
+
+class _ActionListItem {
+  final String text;
+  final Color color;
+  final IconData icon;
+  _ActionListItem({@required this.text, @required this.color, @required this.icon});
 }
