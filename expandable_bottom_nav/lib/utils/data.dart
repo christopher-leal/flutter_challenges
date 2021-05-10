b64ad87746bf5cf179ce84cfcb80ce6673a09f45
Christopher Leal <chris_t_o@hotmail.com>
2021-05-10 17:07:36 -0500

    data modified

--- expandable_bottom_nav/lib/utils/data.dart
+++ expandable_bottom_nav/lib/utils/data.dart
@@ -2,16 +2,16 @@ import 'package:expandable_bottom_nav/models/item.dart';
 import 'package:faker/faker.dart';
 
 const _bgs = [
-  'img1-min.jpg',
-  'img2-min.jpg',
-  'img3-min.jpg',
-  'img4-min.jpg',
+  'img1.png',
+  'img2.png',
+  'img3.png',
+  'img4.png',
 ];
 const _avatars = [
-  'person1-min.jpg',
-  'person2-min.jpg',
-  'person3-min.jpg',
-  'person4-min.jpg',
+  'person1.png',
+  'person2.png',
+  'person3.png',
+  'person4.png',
 ];
 
 List<Item> get fakerItems {
