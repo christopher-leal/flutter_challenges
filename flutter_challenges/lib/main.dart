18c05a19bcddc7785e072498ab87b1f4a65f043c
Christopher Leal <chris_t_o@hotmail.com>
2021-05-11 18:13:23 -0500

    list of challengues applied

--- flutter_challenges/lib/main.dart
+++ flutter_challenges/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_challenges/challenges/bancomer_redesign/bancomer_redesign_main.dart';
+import 'package:flutter_challenges/utils/utils.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +8,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Challenges',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+      theme: ThemeData.dark(),
+      home: _BuildListMenu(),
+    );
+  }
+}
+
+class _BuildListMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Proyectos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MaterialButton(
+              color: Colors.deepPurple,
+              padding: const EdgeInsets.all(15.0),
+              child: Text('Bancomer Redesign'),
+              onPressed: () {
+                Utils.navigateTo(context, BancomerRedesignMain());
+              },
+            )
+          ],
         ),
       ),
     );
