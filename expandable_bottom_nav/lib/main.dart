f98c1ec2b0839fbc1bfdf1cb651a76e59d7bc26f
Christopher Leal <chris_t_o@hotmail.com>
2021-05-10 19:19:58 -0500

    home done

--- expandable_bottom_nav/lib/main.dart
+++ expandable_bottom_nav/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expandable_bottom_nav/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,8 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Expandable bottom nav',
-      home: ,
+      home: HomePage(),
     );
   }
 }
